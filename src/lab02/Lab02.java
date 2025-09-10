a035d730775d1c73f925520ff10e2f81c7d763ee
DavidAyoubVanier <davidayoub.rwa@gmail.com>
2025-09-10 10:50:27 -0400

    Initialization of Stage and Scene

--- src/lab02/Lab02.java
+++ src/lab02/Lab02.java
@@ -5,6 +5,8 @@
 package lab02;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 /**
@@ -17,12 +19,29 @@ public class Lab02 extends Application{
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        launch(args);
     }
 
     @Override
-    public void start(Stage stage) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public void start(Stage primaryStage) {
+        // Set the stage title
+        primaryStage.setTitle("Java Games");
+        
+        // Show the window
+        BorderPane root = new BorderPane();
+        Scene s = new Scene(root, 250, 300);
+        primaryStage.setScene(s);
+        primaryStage.show();
+
+        
+        
+        
+        
+        /* Label toplabel = new Label("Random Game");
+        
+        root.add(toplabel, 0, 0);
+        
+        */
     }
     
 }
